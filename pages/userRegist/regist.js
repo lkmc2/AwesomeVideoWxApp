5e35185b68dee0810c603ecc6a9bcb2091ceb07a
lkmc2 <lkmc2@163.com>
2018-09-29 22:02:04 +0800

    完善注册逻辑

--- pages/userRegist/regist.js
+++ pages/userRegist/regist.js
@@ -1,39 +1,56 @@
 const app = getApp()
 
 Page({
-    data: {
-      
-    },
-    doRegist: (event) => {
-      let formObject = event.detail.value;
-      let username = formObject.username;
-      let password = formObject.password;
+  data: {
 
-      // 简单验证
-      if (username.length == 0 || password.length == 0) {
-        wx.showToast({
-          title: '用户名或密码不能为空',
-          icon: 'none',
-          duration: 3000
-        })
-      } else {
-        let serverUrl = app.serverUrl;
+  },
+  doRegist: (event) => {
+    let formObject = event.detail.value;
+    let username = formObject.username;
+    let password = formObject.password;
 
-        // 请求后台注册接口
-        wx.request({
-          url: serverUrl + '/register',
-          method: "POST",
-          data: {
-            username,
-            password
-          },
-          header: {
-            'content-type': 'application/json'
-          },
-          success: (res) => {
-            console.log(res.data)
+    // 简单验证
+    if (username.length == 0 || password.length == 0) {
+      wx.showToast({
+        title: '用户名或密码不能为空',
+        icon: 'none',
+        duration: 3000
+      })
+    } else {
+      let serverUrl = app.serverUrl;
+
+      // 请求后台注册接口
+      wx.request({
+        url: serverUrl + '/register',
+        method: "POST",
+        data: {
+          username,
+          password
+        },
+        header: {
+          'content-type': 'application/json'
+        },
+        success: (res) => {
+          // console.log(res.data)
+          let status = res.data.status;
+
+          if (status == 200) {
+            wx.showToast({
+              title: '用户注册成功',
+              icon: 'none',
+              duration: 3000
+            })
+
+            app.userInfo = res.data.data;
+          } else if (status == 500) {
+            wx.showToast({
+              title: res.data.msg,
+              icon: 'none',
+              duration: 3000
+            })
           }
-        })
-      }
+        }
+      })
     }
+  }
 })
