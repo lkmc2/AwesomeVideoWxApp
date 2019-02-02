499113ad6b17b187119f2f0b297b431a12a6e44b
lkmc2 <lkmc2@163.com>
2019-02-02 23:51:25 +0800

    设置首页视频分页配置

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,7 +3,12 @@ const app = getApp()
 
 Page({
   data: {
-    screenWidth: 350 // 屏幕宽度
+    // 用于分页的属性
+    totalPage: 1, // 总页数
+    currentPage: 1, // 当前页数
+    videoList: [], // 视频列表
+    screenWidth: 350, // 屏幕宽度
+    serverUrl: '' // 服务器地址
   },
   // 页面加载
   onLoad: function (params) {
@@ -14,5 +19,26 @@ Page({
     that.setData({
       screenWidth: screenWidth,
     });
+
+    // 获取当前页数
+    const currentPage = that.data.currentPage;
+    const serverUrl = app.serverUrl;
+
+    // 显示进度条
+    wx.showLoading({
+      title: '加载中…'
+    })
+
+    // 请求视频分页数据
+    wx.request({
+      url: serverUrl + '/video/showAll?currentPage=' + currentPage,
+      method: 'POST',
+      success: function(res) {
+        // 隐藏进度条
+        wx.hideLoading();
+
+        console.log(res)
+      }
+    })
   }
 })
