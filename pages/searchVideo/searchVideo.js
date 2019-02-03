c8f36137220bdba1abe539ef85ae4bd9ca163162
lkmc2 <lkmc2@163.com>
2019-02-03 18:47:44 +0800

    对第三方搜索框插件进行配置

--- pages/searchVideo/searchVideo.js
+++ pages/searchVideo/searchVideo.js
@@ -1,4 +1,6 @@
 // 视频搜索页
+const WxSearch = require('../../wxSearchView/wxSearchView.js');
+
 const app = getApp()
 
 Page({
@@ -6,7 +8,39 @@ Page({
   },
   // 页面加载
   onLoad: function (params) {
-    var that = this;
+    const that = this;
 
+    // 初始化搜索框
+    WxSearch.init(
+      that,  // 本页面一个引用
+      ['杭州', '嘉兴', "海宁", "桐乡", '宁波', '金华'], // 热点搜索推荐，[]表示不使用
+      [],// 搜索匹配，[]表示不使用
+      that.mySearchFunction, // 提供一个搜索回调函数
+      that.myGobackFunction //提供一个返回回调函数
+    );
+  },
+  // [搜索框] 转发函数，固定部分，直接拷贝即可
+  wxSearchInput: WxSearch.wxSearchInput,  // 输入变化时的操作
+  wxSearchKeyTap: WxSearch.wxSearchKeyTap,  // 点击提示或者关键字、历史记录时的操作
+  wxSearchDeleteAll: WxSearch.wxSearchDeleteAll, // 删除所有的历史记录
+  wxSearchConfirm: WxSearch.wxSearchConfirm,  // 搜索函数
+  wxSearchClear: WxSearch.wxSearchClear,  // 清空函数
+
+  // [搜索框] 搜索回调函数  
+  mySearchFunction: function (value) {
+    // do your job here
+    // 示例：跳转
+    wx.redirectTo({
+      url: '../index/index?searchValue=' + value
+    })
+  },
+
+  // [搜索框] 返回回调函数
+  myGobackFunction: function () {
+    // do your job here
+    // 示例：返回
+    wx.redirectTo({
+      url: '../index/index?searchValue=返回'
+    })
   }
 })
